542f7da65bf2662eb58fecada409de6c100d2949
Callum Neale <callum@Callums-MacBook-Air.local>
2023-06-01 09:25:05 +0100

    remove console logs

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,25 +8,19 @@ export const getArticles = async () => {
   try {
     const { data } = await api.get("/articles");
     return data;
-  } catch (error) {
-    console.log(error);
-  }
+  } catch (error) {}
 };
 
 export const getArticle = async (article_id) => {
   try {
     const { data } = await api.get(`/articles/${article_id}`);
     return data;
-  } catch (error) {
-    console.log(error);
-  }
+  } catch (error) {}
 };
 
 export const getComments = async (article_id) => {
   try {
     const { data } = await api.get(`/articles/${article_id}/comments`);
     return data;
-  } catch (error) {
-    console.log(error);
-  }
+  } catch (error) {}
 };
